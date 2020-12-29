10bae673656e914b8038a2e181b7b4b7d4c7cd7c
Pial Rahman <pial56@outlook.com>
2020-12-29 23:01:21 +0600

    Registration Option added

---
+++ Frontend-JQuery/myscript.js
@@ -0,0 +1,53 @@
+$(document).ready(function(){
+	$("#adduser").click(function(){
+		Registration();
+	});
+	var Userlist=function(){
+	$.ajax({
+		url:"http://localhost:9799/api/userregistration",
+		method:"GET",
+		complete:function(xmlhttp,status){
+			if(xmlhttp.status==200)
+			{
+				var data=xmlhttp.responseJSON;
+			    var uslst='';
+			    for(var i=0;i<data.length;i++)
+				{
+					uslst+="<tr><td>"+data[i].UId+"</td><td>"+data[i].UEmail+"</td><td>"+data[i].Uname+"</td><tr>";
+					
+					//strspcfcpst+="<option value="+data[i].Post1+">"+data[i].PostID+"</option>";
+			}
+			$("#UsersList tbody").html(uslst);	
+			}
+            else{
+            	$("#uinmsg").html(xmlhttp.status+":"+xmlhttp.statusText);
+            }
+        }
+		});
+}
+Userlist();
+
+var Registration=function(){
+	$.ajax({
+		url:"http://localhost:9799/api/userregistration",
+		method:"POST",
+		header:"Content-Type:application/jason",
+		data:{
+			UEmail:$("#useremail").val(),
+            Uname:$("#username").val(),
+            Upassword:$("#passwords").val(),
+		},
+		complete:function(xmlhttp,status){
+			if(xmlhttp.status==201)
+			{
+				$("#rmz").html("Registration Successful");
+				Userlist();
+				
+			}
+            else{
+            	$("#rmz").html(xmlhttp.status+":"+xmlhttp.statusText);
+            }
+        }
+		});
+}
+});

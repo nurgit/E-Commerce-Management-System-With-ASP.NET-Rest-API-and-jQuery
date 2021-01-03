9cadbbcc50103a39da84cff44e5c816a61ce738d
Nur Mohammad <16-33013-3@student.aiub.edu>
2021-01-03 18:35:39 +0600

    add productbycategoryController

---
+++ Backend-Rest _API/Backend-Rest _API/Controllers/ProductByCategoryController.cs
@@ -0,0 +1,30 @@
+﻿using Backend_Rest__API.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Backend_Rest__API.Controllers
+{
+    [RoutePrefix("api/categorys/{id}/products")]
+    public class ProductByCategoryController : ApiController
+    {
+        ProductRepository productRepository = new ProductRepository();
+        CategoryRepository categoryRepository = new CategoryRepository();
+        // Get all Products by Categories 
+        [Route("")]
+        public IHttpActionResult Get(int id)
+        {
+
+            return Ok(productRepository.GetProductsByCategory(id));
+        }
+
+
+        //Get a Specific products from category 
+        
+
+
+    }
+}
--- Backend-Rest _API/Backend-Rest _API/Repository/ProductRepository.cs
+++ Backend-Rest _API/Backend-Rest _API/Repository/ProductRepository.cs
@@ -8,5 +8,9 @@ namespace Backend_Rest__API.Repository
 {
     public class ProductRepository: Repository<Tbl_Product>
     {
+        public List<Tbl_Product> GetProductsByCategory(int id)
+        {
+            return this.GetAll().Where(X => X.CategoryId == id).ToList();
+        }
     }
 }

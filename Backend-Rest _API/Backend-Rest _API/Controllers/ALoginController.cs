36546a66628113506941356fba9cdc27db832d22
Nur Mohammad <16-33013-3@student.aiub.edu>
2021-01-03 23:24:01 +0600

    add ulogin and alogin

---
+++ Backend-Rest _API/Backend-Rest _API/Controllers/ALoginController.cs
@@ -0,0 +1,28 @@
+﻿using Backend_Rest__API.Models;
+using Backend_Rest__API.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Backend_Rest__API.Controllers
+{
+    [RoutePrefix("api/alogin")]
+    public class ALoginController : ApiController
+    {
+      
+        AdminLoginRepository adminLoginRepository = new AdminLoginRepository();
+        [Route("")]
+        public IHttpActionResult Post(AdminLogin Admin)
+        {
+            AdminLogin logggedInUser = adminLoginRepository.GetAll().Where(s => s.AUserName == Admin.AUserName && s.APassword == Admin.APassword).FirstOrDefault();
+            if (logggedInUser != null)
+            {
+                return Ok(logggedInUser);
+            }
+            return StatusCode(HttpStatusCode.Unauthorized);
+        }
+    }
+}
---
+++ Backend-Rest _API/Backend-Rest _API/Controllers/ULoginController.cs
@@ -0,0 +1,27 @@
+﻿using Backend_Rest__API.Models;
+using Backend_Rest__API.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Backend_Rest__API.Controllers
+{
+    [RoutePrefix("api/ulogin")]
+    public class ULoginController : ApiController
+    {
+        UserRegistrationRepository userRegistrationRepository = new UserRegistrationRepository();
+        [Route("")]
+        public IHttpActionResult Post(UserRegistration user)
+        {
+            UserRegistration logggedInUser = userRegistrationRepository.GetAll().Where(s => s.Uname == user.Uname && s.Upassword == user.Upassword).FirstOrDefault();
+            if (logggedInUser != null)
+            {
+                return Ok(logggedInUser);
+            }
+            return StatusCode(HttpStatusCode.Unauthorized);
+        }
+    }
+}
